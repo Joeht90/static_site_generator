b5a8ae5a7edee91df36fc19866e4133189150ff1
joeht90 <joeht90@icloud.com>
2025-05-06 12:19:29 -0700

    stuff was done

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,23 +1,20 @@
-class HTMLNode():
-
-
+class HTMLNode:
     def __init__(self, tag=None, value=None, children=None, props=None):
         self.tag = tag
         self.value = value
-        self.children = children
-        self.props = props
-
-
-    def to_html(self):
-        raise NotImplementedError
-    
+        self.children = children if children is not None else []
+        self.props = props if props is not None else {}
 
     def props_to_html(self):
-        return f' {self.tag}="{self.value}" {self.target}="{self.props}"'
-    
+        """Generate the HTML attributes for the node as a string."""
+        return ' '.join([f'{key}="{value}"' for key, value in self.props.items()])
 
     def __repr__(self):
-        print(self.props_to_html)
+        """Return a string representation of the node."""
+        return f"HTMLNode(tag={self.tag}, value={self.value}, children={self.children}, props={self.props})"
 
+    def to_html(self):
+        """This method should be overridden by child classes."""
+        raise NotImplementedError("to_html() must be implemented by subclasses.")
 
     
