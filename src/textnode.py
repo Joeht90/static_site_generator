49403dfc7aec9a3a9b8555d8aeafcd1dd704823c
joeht90 <joeht90@icloud.com>
2025-05-10 20:46:13 -0700

    inline markdown addition

--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from htmlnode import LeafNode
 
 
 class TextType(Enum):
@@ -15,7 +16,6 @@ class TextNode():
         self.text = text
         self.text_type = text_type
         self.url = url
-       
         
     def __eq__(self, other):
         return (
@@ -24,6 +24,22 @@ class TextNode():
             self.url == other.url
         )
         
-    
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
+    
+
+def text_node_to_html_node(text_node):
+    if text_node.text_type == TextType.TEXT:
+        return LeafNode(tag=None, value=text_node.text)
+    if text_node.text_type == TextType.BOLD:
+        return LeafNode(tag="b", value=text_node.text)
+    if text_node.text_type == TextType.ITALIC:
+        return LeafNode(tag="i", value=text_node.text)
+    if text_node.text_type == TextType.CODE:
+        return LeafNode(tag="code", value=text_node.text)
+    if text_node.text_type == TextType.LINK:
+        return LeafNode(tag="a", value=text_node.text, props={"href": text_node.url})
+    if text_node.text_type == TextType.IMAGE:
+        return LeafNode(tag="img", value="", props={"src": text_node.url, "alt": text_node.text})
+    raise ValueError(f"invalid text type: {text_node.text_type}")
+
